883bb77c76e0962a6a2d2af5da5ac27e885bbd43
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-09-03 20:49:44 +0600

    bot message send

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 const token = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (option) => {
+  //   console.log("hello my name is osman goni ", option);
+  bot.sendMessage(option.chat.id, "hello everyone i am osman goni rabbi ");
+});
